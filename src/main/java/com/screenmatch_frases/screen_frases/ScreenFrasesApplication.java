20aad7a89d63e0fec91053b70f1b482f1f78ff55
jonhvtr <jonhvtr_dev@hotmail.com>
2025-07-22 11:22:41 -0300

    Initial Commit

---
+++ src/main/java/com/screenmatch_frases/screen_frases/ScreenFrasesApplication.java
@@ -0,0 +1,13 @@
+package com.screenmatch_frases.screen_frases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenFrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenFrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/screenmatch_frases/screen_frases/ScreenFrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.screenmatch_frases.screen_frases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenFrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
